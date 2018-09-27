5fb27fbb68ef5ed0adecce03e0639e2eb1e4ab48
garsue <osuga.shunya@mfkessai.co.jp>
2018-09-28 01:06:14 +0900

    Use gorm

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
-	"gopkg.in/urfave/cli.v1"
 	"log"
 	"os"
 
 	_ "github.com/garsue/sparql/sql"
+	"github.com/jinzhu/gorm"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
@@ -31,7 +31,7 @@ func search(ctx context.Context, name string) (err error) {
 		return errors.New("specify office name")
 	}
 
-	db, err := sql.Open("sparql", "https://api.hojin-info.go.jp/sparql")
+	db, err := gorm.Open("sparql", "https://api.hojin-info.go.jp/sparql")
 	if err != nil {
 		return err
 	}
@@ -41,34 +41,28 @@ func search(ctx context.Context, name string) (err error) {
 		}
 	}()
 
+	var hojins []struct {
+		ID   uint64
+		Name string
+	}
+
 	//noinspection SqlNoDataSourceInspection
-	rows, err := db.Query(`
+	if err = db.Raw(`
 PREFIX hj: <http://hojin-info.go.jp/ns/domain/biz/1#>
 PREFIX ic: <http://imi.go.jp/ns/core/rdf#>
 	
-SELECT ?v,?i FROM <http://hojin-info.go.jp/graph/hojin>
+SELECT ?id,?name FROM <http://hojin-info.go.jp/graph/hojin>
 WHERE {
 	?s hj:法人基本情報 ?n .
-	?n ic:名称/ic:表記 ?v ;
-	   ic:ID/ic:識別値 ?i .
-	FILTER regex(?v, $1)
-} LIMIT 100`, name)
-	if err != nil {
+	?n ic:ID/ic:識別値 ?id .
+	?n ic:名称/ic:表記 ?name .
+	FILTER regex(?name, $1)
+} LIMIT 100`, name).Find(&hojins).Error; err != nil {
 		return err
 	}
-	defer func() {
-		if err2 := rows.Close(); err2 != nil {
-			err = err2
-		}
-	}()
 
-	for rows.Next() {
-		var v string
-		var i string
-		if err := rows.Scan(&v, &i); err != nil {
-			return err
-		}
-		fmt.Println(v, i)
+	for _, h := range hojins {
+		fmt.Println(h.ID, h.Name)
 	}
 
 	return nil
